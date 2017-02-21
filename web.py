eb16184e7229826f3507a973404010804237992e
Pedro Calero <pedrocalero@ieslallitera.com>
2017-02-21 12:26:07 +0100

    Busca el texto y devuelve las fechas

--- web.py
+++ web.py
@@ -7,6 +7,7 @@ Client web per ww.udl.cat
 @author: pedrocalero@ieslallitera.com
 '''
 import urllib2
+from bs4 import BeautifulSoup
 
 
 class Client(object):
@@ -17,11 +18,20 @@ class Client(object):
 		f.close()
 		return html
 
+	# buscar el texto
+	def search_text(self, html):
+		soup = BeautifulSoup(html, 'html.parser')
+		elements = soup.find_all("div", "featured-links-item")
+		resultats = []
+		for element in elements:
+			resultats.append(element.find("time")["datetime"])
+		return resultats
+
 	def main(self):
 		web = self.get_web('http://www.udl.cat/')
-		# buscar el texto
-		# imprimir resultados
-		print web
+		resultat = self.search_text(web)
+		# TODO: imprimir resultados
+		print resultat
 
 if __name__ == "__main__":
 	client = Client()
